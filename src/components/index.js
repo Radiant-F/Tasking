1d8c94bd930bd308fc7684b64efb282696258df4
Radiant-F <radiantfadilah0@gmail.com>
2023-10-25 12:41:32 +0700

    uninstall unecessary  package

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from './Header';
-import Task from './Task';
-import TaskInput from './TaskInput';
-import ModalEditTask from './ModalEditTask';
-
-export {Header, TaskInput, Task, ModalEditTask};
